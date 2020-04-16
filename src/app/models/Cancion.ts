4bc739680ce88d047a7c16f40546b62813719f09
Ana Real <48548722+anarealt@users.noreply.github.com>
2020-04-16 20:01:20 +0200

    Componente Canción

---
+++ src/app/models/Cancion.ts
@@ -0,0 +1,16 @@
+export interface Cancion {
+  artistName: string;
+  id: string;
+  releaseDate?: string;
+  name: string;
+  duration?: number;
+  collectionName: string;
+  kind: string;
+  copyright: string;
+  artistId: string;
+  contentAdvisoryRating?: string;
+  artistUrl: string;
+  artworkUrl: string;
+  genres: any;
+  url: string;
+}
